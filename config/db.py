86c41782d16368f4a080cbe7df836c84282e3260
Daryl <darylcs2325@gmail.com>
2022-12-20 16:46:13 -0500

    'modificacion'

--- config/db.py
+++ config/db.py
@@ -4,7 +4,7 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config/cfg.ini')
 postgres = config['POSTGRES']['CONNSTR']
-
+print(postgres)
 meta = MetaData()
 
 engine = create_engine(postgres)
